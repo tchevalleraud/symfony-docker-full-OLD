a0402c86f6a14fe3c8ad6684b4ae658570e74ee6
Thibault Chevalleraud <thibault.chevalleraud@gmail.com>
2021-07-29 10:37:09 +0200

    Update workflow #11

--- tests/UI/FrontOffice/Panther/DashboardTest.php
+++ tests/UI/FrontOffice/Panther/DashboardTest.php
@@ -8,11 +8,15 @@
 
         public function test_EN_Index(){
             $client = self::createPantherClient([
-                'hostname'  => '192.168.0.249:10101'
+                'external_base_uri' => 'http://192.168.0.249:10101'
             ]);
             $client->manage()->window()->setSize(new WebDriverDimension(1920, 1080));
             $client->request("GET", "/en/dashboard.html");
+            
             $client->takeScreenshot('screenshot/1920x1080/test.png');
+
+            $this->assertStringContainsString("/en/dashboard.html", $client->getCurrentURL());
+            $this->assertSelectorTextContains("h1", "Dashboard");
         }
 
     }
