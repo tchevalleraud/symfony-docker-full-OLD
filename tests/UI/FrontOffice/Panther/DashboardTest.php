344f67d6cc7da81f25131622394436bb6ed815b5
Thibault Chevalleraud <thibault.chevalleraud@gmail.com>
2021-07-29 10:15:29 +0200

    Update workflow #8

--- tests/UI/FrontOffice/Panther/DashboardTest.php
+++ tests/UI/FrontOffice/Panther/DashboardTest.php
@@ -1,6 +1,16 @@
 <?php
     namespace App\Tests\UI\FrontOffice\Panther;
 
-    class DashboardTest {
+    use Facebook\WebDriver\WebDriverDimension;
+    use Symfony\Component\Panther\PantherTestCase;
+
+    class DashboardTest extends PantherTestCase {
+
+        public function test_EN_Index(){
+            $client = static::createPantherClient();
+            $client->manage()->window()->setSize(new WebDriverDimension(1920, 1080));
+            $client->request("GET", "/en/dashboard.html");
+            $this->assertSelectorTextContains('h1', 'Dashboard');
+        }
 
     }
