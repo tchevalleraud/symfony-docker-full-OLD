5690dc4977bc88f7f5c9aca1e2c165dbff629f7c
Thibault Chevalleraud <thibault.chevalleraud@gmail.com>
2021-07-29 10:19:44 +0200

    Update workflow #9

--- tests/UI/FrontOffice/Panther/DashboardTest.php
+++ tests/UI/FrontOffice/Panther/DashboardTest.php
@@ -7,7 +7,10 @@
     class DashboardTest extends PantherTestCase {
 
         public function test_EN_Index(){
-            $client = static::createPantherClient();
+            $client = static::createPantherClient([
+                'hostname'  => 'http://nginx',
+                'port'      => 10101
+            ]);
             $client->manage()->window()->setSize(new WebDriverDimension(1920, 1080));
             $client->request("GET", "/en/dashboard.html");
             $this->assertSelectorTextContains('h1', 'Dashboard');
